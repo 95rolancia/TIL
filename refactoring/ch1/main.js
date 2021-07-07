1658733b8cbd4f640c4fb3d95bf4ff2a0454f501
jangjunhyeok <95jjangjun@gmail.com>
2021-07-08 00:04:39 +0900

    playFor() 옮기기

--- refactoring/ch1/main.js
+++ refactoring/ch1/main.js
@@ -4,12 +4,16 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
-  console.log(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사
+    result.play = playFor(result);
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
@@ -17,7 +21,7 @@ function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
     // 청구 내역 출력
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -25,13 +29,9 @@ function renderPlainText(data, plays) {
   return result;
 
   // nested Function
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -46,7 +46,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르 ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르 ${aPerformance.play.type}`);
     }
     return result;
   }
@@ -55,7 +55,7 @@ function renderPlainText(data, plays) {
     let result = 0;
     result += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공
-    if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+    if ("comedy" === perf.play.type) result += Math.floor(perf.audience / 5);
     return result;
   }
 
