8d984a396c0a78eee8c3ac0886fabb335e7319c6
jangjunhyeok <95jjangjun@gmail.com>
2021-07-07 23:39:53 +0900

    totalAmount 반복문 쪼개기

--- refactoring/ch1/main.js
+++ refactoring/ch1/main.js
@@ -7,8 +7,12 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     // 청구 내역 출력
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+  }
+
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
+
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
